aeb7c6030f308b144289d8092c099be6f8b6e058
ocasergi@gmail.com <ocasergi@gmail.com>
2020-05-15 14:55:30 +0200

    smallest common

--- script.js
+++ script.js
@@ -640,7 +640,30 @@ function addTogether(...args) {
 addTogether(2)(3);
   
 /////////////////////////////////////////////////////////
+// Find the smallest common multiple of the provided parameters that can be evenly divided by both, as well as by all sequential numbers in the range between these parameters.
+
+function smallestCommons(arr) {
+
+  arr.sort(function(a, b) {
+    return b - a;
+  });
+  const smallest = arr[1];
+  const biggest = arr[0];
+  let solution = biggest;
+
+  for(let i = biggest; i >= smallest; i--) {
+    if(solution % i) {
+      solution += biggest;
+      i = biggest;
+    }
+  }
+  return solution;
+}
+
+smallestCommons([1,5]);
       
+/////////////////////////////////////////////////////////
+
 //Convert to Roman
 function convertToRoman(num) {
 
